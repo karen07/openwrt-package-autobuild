1355f1bc9d529e436d4a5f0e996bbb9363016c49
Khachatryan Karen <karen0734@gmail.com>
2025-07-28 10:57:27 +0300

    Format sh

--- auto_install.sh
+++ auto_install.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 
 if [ "$1" != "antiblock" ] && [ "$1" != "yubikey-hack" ] && [ "$1" != "dns-client-test" ] && [ "$1" != "domains-block-test" ] && [ "$1" != "dns-server-test" ] && [ "$1" != "luci-app-antiblock" ]; then
-	echo "Argument 1: Invalid package name"
-	echo "Use antiblock or yubikey-hack or dns-client-test or domains-block-test or dns-server-test or luci-app-antiblock"
-	exit 1
+    echo "Argument 1: Invalid package name"
+    echo "Use antiblock or yubikey-hack or dns-client-test or domains-block-test or dns-server-test or luci-app-antiblock"
+    exit 1
 fi
 
 if [ -z "$2" ]; then
-	echo "Argument 2: Empty router ssh name"
-	exit 1
+    echo "Argument 2: Empty router ssh name"
+    exit 1
 fi
 
 if [ -f /usr/bin/apt ]; then
-	sudo apt update && sudo apt-get install -y make unzip bzip2 build-essential libncurses5-dev libncursesw5-dev
+    sudo apt update && sudo apt-get install -y make unzip bzip2 build-essential libncurses5-dev libncursesw5-dev
 fi
 
 if [ -f /usr/bin/pacman ]; then
-	sudo pacman -S make wget rsync base-devel unzip python3 python-distutils-extra --noconfirm
+    sudo pacman -S make wget rsync base-devel unzip python3 python-distutils-extra --noconfirm
 fi
 
 PACKAGE_NAME=$1
 ROUTER_NAME=$2
 
 if ! ssh -o StrictHostKeyChecking=no $ROUTER_NAME cat /etc/os-release; then
-	echo "SSH connection or remote command failed"
-	exit 1
+    echo "SSH connection or remote command failed"
+    exit 1
 fi
 
 ROUTER_OS_RELEASE="$(ssh $ROUTER_NAME cat /etc/os-release)"
@@ -34,13 +34,13 @@ SDK_WEB_FOLDER="https://downloads.openwrt.org/releases/$VERSION/targets/$BOARD/"
 SDK_ARCHIVE=$(curl -s $SDK_WEB_FOLDER | grep -i sdk | sed -r 's/.*href="([^"]+).*/\1/g')
 
 if [ ! -f "$SDK_ARCHIVE" ]; then
-	wget $SDK_WEB_FOLDER$SDK_ARCHIVE
+    wget $SDK_WEB_FOLDER$SDK_ARCHIVE
 fi
 
 SDK_FOLDER=$(echo $SDK_ARCHIVE | rev | cut -d'.' -f2- | cut -d'.' -f2- | rev)
 
 if [ ! -d "$SDK_FOLDER" ]; then
-	tar -xf $SDK_ARCHIVE
+    tar -xf $SDK_ARCHIVE
 fi
 
 cd $SDK_FOLDER/
@@ -51,19 +51,19 @@ cd $SDK_FOLDER/
 ssh -o StrictHostKeyChecking=no git@github.com
 
 if [ "$PACKAGE_NAME" != "luci-app-antiblock" ]; then
-	if [ ! -d "package/$PACKAGE_NAME" ]; then
-		if ! git clone --recursive git@github.com:karen07/$PACKAGE_NAME.git package/$PACKAGE_NAME; then
-			git clone --recursive https://github.com/karen07/$PACKAGE_NAME.git package/$PACKAGE_NAME
-		fi
-	fi
+    if [ ! -d "package/$PACKAGE_NAME" ]; then
+        if ! git clone --recursive git@github.com:karen07/$PACKAGE_NAME.git package/$PACKAGE_NAME; then
+            git clone --recursive https://github.com/karen07/$PACKAGE_NAME.git package/$PACKAGE_NAME
+        fi
+    fi
 fi
 
 PACKAGE_BUILD_NAME=$PACKAGE_NAME-openwrt-package
 
 if [ ! -d "package/$PACKAGE_BUILD_NAME" ]; then
-	if ! git clone --recursive git@github.com:karen07/$PACKAGE_BUILD_NAME.git package/$PACKAGE_BUILD_NAME; then
-		git clone --recursive https://github.com/karen07/$PACKAGE_BUILD_NAME.git package/$PACKAGE_BUILD_NAME
-	fi
+    if ! git clone --recursive git@github.com:karen07/$PACKAGE_BUILD_NAME.git package/$PACKAGE_BUILD_NAME; then
+        git clone --recursive https://github.com/karen07/$PACKAGE_BUILD_NAME.git package/$PACKAGE_BUILD_NAME
+    fi
 fi
 
 cat >auto.sh <<EOF
