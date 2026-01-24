7ae26d140b4ed1e88a557aee1d3b49fdbe7d2c82
Khachatryan Karen <karen0734@gmail.com>
2026-01-24 13:22:09 +0300

    Fix

--- auto_install.sh
+++ auto_install.sh
@@ -2,9 +2,11 @@
 
 if [ "$1" != "antiblock" ] && [ "$1" != "yubikey-hack" ] \
     && [ "$1" != "dns-client-test" ] && [ "$1" != "domains-block-test" ] \
-    && [ "$1" != "dns-server-test" ] && [ "$1" != "luci-app-antiblock" ] && [ "$1" != "QUICTun" ]; then
+    && [ "$1" != "dns-server-test" ] && [ "$1" != "luci-app-antiblock" ] \
+    && [ "$1" != "QUICTun" ]; then
     echo "Argument 1: Invalid package name"
-    echo "Use antiblock or yubikey-hack or dns-client-test or domains-block-test or dns-server-test or luci-app-antiblock or QUICTun"
+    echo "Use antiblock or yubikey-hack or dns-client-test or domains-block-test \
+or dns-server-test or luci-app-antiblock or QUICTun"
     exit 1
 fi
 
@@ -15,11 +17,28 @@ fi
 
 if [ -f /usr/bin/apt ]; then
     sudo apt update
-    sudo apt install -y build-essential libncurses-dev git rsync swig unzip zstd wget
+    sudo apt install -y \
+        build-essential \
+        libncurses-dev \
+        git \
+        rsync \
+        swig \
+        unzip \
+        zstd \
+        wget
 fi
 
 if [ -f /usr/bin/pacman ]; then
-    sudo pacman -Sy --noconfirm base-devel python python-setuptools git rsync swig unzip zstd wget
+    sudo pacman -Sy --noconfirm \
+        base-devel \
+        python \
+        python-setuptools \
+        git \
+        rsync \
+        swig \
+        unzip \
+        zstd \
+        wget
 fi
 
 PACKAGE_NAME="$1"
@@ -31,16 +50,30 @@ if ! ssh -o StrictHostKeyChecking=no "$ROUTER_NAME" cat /etc/os-release; then
 fi
 
 ROUTER_OS_RELEASE="$(ssh "$ROUTER_NAME" cat /etc/os-release)"
-VERSION=$(echo "$ROUTER_OS_RELEASE" | grep VERSION | head -n 1 | cut -d'"' -f2)
-BOARD=$(echo "$ROUTER_OS_RELEASE" | grep OPENWRT_BOARD | head -n 1 | cut -d'"' -f2)
-SDK_WEB_FOLDER="https://downloads.openwrt.org/releases/$VERSION/targets/$BOARD/"
-SDK_ARCHIVE=$(curl -s "$SDK_WEB_FOLDER" | grep -i sdk | awk 'match($0, /href="([^"]+)"/, a) { print a[1] }')
+
+VERSION=$(echo "$ROUTER_OS_RELEASE" \
+    | grep VERSION | head -n 1 | cut -d'"' -f2)
+
+BOARD=$(echo "$ROUTER_OS_RELEASE" \
+    | grep OPENWRT_BOARD | head -n 1 | cut -d'"' -f2)
+
+OPENWRT_DL="https://downloads.openwrt.org/releases"
+SDK_WEB_FOLDER="$OPENWRT_DL/$VERSION/targets/$BOARD/"
+
+SDK_ARCHIVE=$(
+    curl -s "$SDK_WEB_FOLDER" \
+        | grep -i sdk \
+        | awk 'match($0, /href="([^"]+)"/, a) { print a[1] }'
+)
 
 if [ ! -f "$SDK_ARCHIVE" ]; then
     wget "${SDK_WEB_FOLDER}${SDK_ARCHIVE}"
 fi
 
-SDK_FOLDER=$(echo "$SDK_ARCHIVE" | rev | cut -d'.' -f2- | cut -d'.' -f2- | rev)
+SDK_FOLDER=$(
+    echo "$SDK_ARCHIVE" \
+        | rev | cut -d'.' -f2- | cut -d'.' -f2- | rev
+)
 
 if [ ! -d "$SDK_FOLDER" ]; then
     tar -xf "$SDK_ARCHIVE"
@@ -55,8 +88,12 @@ ssh -o StrictHostKeyChecking=no git@github.com
 
 if [ "$PACKAGE_NAME" != "luci-app-antiblock" ]; then
     if [ ! -d "package/$PACKAGE_NAME" ]; then
-        if ! git clone --recursive "git@github.com:karen07/$PACKAGE_NAME.git" "package/$PACKAGE_NAME"; then
-            git clone --recursive "https://github.com/karen07/$PACKAGE_NAME.git" "package/$PACKAGE_NAME"
+        if ! git clone --recursive \
+            "git@github.com:karen07/$PACKAGE_NAME.git" \
+            "package/$PACKAGE_NAME"; then
+            git clone --recursive \
+                "https://github.com/karen07/$PACKAGE_NAME.git" \
+                "package/$PACKAGE_NAME"
         fi
     fi
 fi
@@ -64,8 +101,12 @@ fi
 PACKAGE_BUILD_NAME="${PACKAGE_NAME}-openwrt-package"
 
 if [ ! -d "package/$PACKAGE_BUILD_NAME" ]; then
-    if ! git clone --recursive "git@github.com:karen07/$PACKAGE_BUILD_NAME.git" "package/$PACKAGE_BUILD_NAME"; then
-        git clone --recursive "https://github.com/karen07/$PACKAGE_BUILD_NAME.git" "package/$PACKAGE_BUILD_NAME"
+    if ! git clone --recursive \
+        "git@github.com:karen07/$PACKAGE_BUILD_NAME.git" \
+        "package/$PACKAGE_BUILD_NAME"; then
+        git clone --recursive \
+            "https://github.com/karen07/$PACKAGE_BUILD_NAME.git" \
+            "package/$PACKAGE_BUILD_NAME"
     fi
 fi
 
